30c2097f604a473c95be967d475c246695a03da2
webomotion <webomotion@de2dd4ef-b379-0410-90dc-d33ebaf511f5>
2009-12-02 12:14:16 +0000

    abstrakte Manager-Klasse: * setzt allgemeine Methoden der Manager-Klassen
    TO-DO:
    * Methode render() ist noch nicht fertig. (Smarty benutzen?)

---
+++ Manager.php
@@ -0,0 +1,40 @@
+<?php
+
+//require_once 'Smarty.php';
+
+abstract class Manager {
+    
+    protected $error = array();
+    
+    const NO_ACTION =
+        'Aktion konnte nicht ausgeführt werden.';
+    
+    public function __construct($action, array $param) {
+        if(method_exists($this, $action)) {
+            $this->$action($param);
+        }
+        else {
+            $this->setError(self::NO_ACTION);
+        }
+    }
+    
+    public function error() {
+        if(count($this->error)>0) {
+            return $this->error;
+        }
+        
+        return false;
+    }
+    
+    protected function render($tpl) {
+        //$smarty = new Smarty();
+        
+        // Liste aller verfügbaren URLs ins Template laden
+        
+        //$smarty->display($tpl);
+    }
+    
+    protected function setError($error) {
+        $this->error[] = $error;
+    }
+} ?>
