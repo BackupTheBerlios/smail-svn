b582148a33957260ef3b31e6a07addaee88a50c8
webomotion <webomotion@de2dd4ef-b379-0410-90dc-d33ebaf511f5>
2009-12-02 12:18:37 +0000

    Account-Manager: * enthält Logik des Account-Webinterface
    TO-DO:
    * Verarbeitung der Sessions (allgemeine Sessionverwaltungs-Klasse?)
    * Angabe der URLs (allgemeine URL-Verwaltungsklasse?)

---
+++ AccountManager.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once 'Account.php';
+require_once 'Manager.php';
+
+class AccountManager extends Manager {
+    
+    const ACCOUNT_EXISTS =
+        'Der Benutzername ist bereits vergeben.';
+    
+    const ACCOUNT_NOT_EXIST =
+        'Benutzername und/oder Password sind falsch.';
+    
+    private function create(array $param) {
+        if(!(isset($param['account']) && isset($param['password']))) {
+            $this->setError(self::NO_ACTION);
+        }
+        else {
+            $account = new Account($param['account'], $param['password']);
+            
+            if(!$account->exist()) {
+                $account->create();
+                //$_SESSION['account'] = $account->getName();
+                
+                //header('Location: '.DOMAIN.'Login/');
+            } else {
+                $this->setError(self::ACCOUNT_EXISTS);
+            }
+        }
+        
+        $this->render();
+    }
+    
+    private function login(array $param) {
+        if(!(isset($param['account']) && isset($param['password']))) {
+            $this->setError(self::NO_ACTION);
+        }
+        else {
+            $account = new Account($param['account'], $param['password']);
+            
+            if($account->exist()) {
+                $account->create($param['password']);
+                //$_SESSION['account'] = $account->getName();
+                
+                //header('Location: '.DOMAIN.'Account/');
+            } else {
+                $this->setError(self::ACCOUNT_NOT_EXISTS);
+            }
+        }
+        
+        $this->render();
+    }
+    
+    private function logout(array $param) {
+        //session_destroy();
+        //unset($_SESSION);
+    }
+} ?>
